10ed0f0258cefb6fedf75170120cb4d55539ce47
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-07-19 11:00:28 +0200

    Create fetchUserInfoAction.js

---
+++ client/src/states/user-loggedin-info/fetchUserInfoAction.js
@@ -0,0 +1,44 @@
+import post from "../../post";
+
+export const fetchUserRequest = () => {
+    return{
+        type : 'FETCH_USER_REQUEST',
+    }
+}
+
+export const fetchUserSuccess = (resp) => {
+    return{
+        type : 'FETCH_USER_SUCCESS',
+        payload : resp
+    }
+}
+
+export const fetchUserFailure = (resp) => {
+    return{
+        type : 'FETCH_USER_FAILURE',
+        payload : resp
+    }
+}
+
+export const fetchUserInfo = (jwt) => {
+    return (dispatch) => {
+        dispatch(fetchUserRequest())
+        post('http://localhost:8080/auth/user',{
+            jwt : jwt
+        }).then(resp => {
+            if (!resp.data.err){
+                dispatch(fetchUserSuccess(resp.data))
+            }else{
+                console.log(resp)
+                dispatch(fetchUserFailure(resp.data))
+            }
+        }).catch(e=>{
+            console.log(e.message)
+        })
+    }
+}
+
+export const logout = () => async (dispatch) => {
+    localStorage.removeItem("jwt")
+    dispatch({type: 'USER_LOGOUT'})
+}
