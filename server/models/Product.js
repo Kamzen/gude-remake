4ae6f84df1d5868daf3a5aa96e6d3906d22e146b
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-07-19 11:03:14 +0200

    Create Product.js

---
+++ server/models/Product.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema({
+        _id: mongoose.Schema.Types.ObjectId,
+        category: {
+            type: String,
+            required: true,
+        },
+        other: {
+            type: String,
+        },
+        product_img_url: {
+            type: Array,
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        desc: {
+            type: String,
+            required: true,
+        },
+        condition: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: String,
+            required: true,
+        },
+        quantity: {
+            type: Number,
+            required: true,
+        },
+        payment_method: {
+            type: String,
+            required: true
+        },
+        _user_id: {
+            type: String,
+            required: true
+        }
+    },
+    {
+        timestamps: true,
+    }
+);
+
+module.exports = mongoose.model('Product', ProductSchema);
