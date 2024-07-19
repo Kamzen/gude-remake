9e1535b41832e2ef45c75917a7ce43971da9e526
Kamzen <37660966+Kamzen@users.noreply.github.com>
2024-07-19 10:21:54 +0200

    Create mobileBottomNav.jsx

---
+++ client/src/gude-mobile/components/mobile-bottomnav/mobileBottomNav.jsx
@@ -0,0 +1,87 @@
+import React, {useState} from "react";
+
+import img from '../../../images/default_img.png';
+import './mobileBottomNav.scss';
+
+import ordersIcon from '../../images/orders-icon.svg';
+import {withRouter} from "react-router-dom";
+
+const MobileBottomNav = ({history}) => {
+
+    const [home, setHome] = useState(true)
+    const [cart, setCart] = useState(false)
+    const [add, setAdd] = useState(false)
+    const [order, setOrder] = useState(false)
+    const [profile, setProfile] = useState(false)
+
+    return (
+        <div className={'mobile-bottom-nav bd'}>
+            <div className={`home ${home ? 'link-active' : ''}`}
+                 onClick={() => {
+                     setHome(true)
+                     setCart(false)
+                     setAdd(false)
+                     setOrder(false)
+                     setProfile(false)
+                     history.push('home')
+                 }}>
+                <i className={'fas fa-home'}> </i>
+                <p className={'home-text'}>Home</p>
+            </div>
+            <div className={`btm-cart ${cart ? 'link-active' : ''}`}
+                 onClick={() => {
+                     setHome(false)
+                     setCart(true)
+                     setAdd(false)
+                     setOrder(false)
+                     setProfile(false)
+                     history.push('/cart')
+                 }}
+            >
+                <i className="material-icons">add_shopping_cart</i>
+                <p>Cart</p>
+            </div>
+            <div className={`add ${add ? 'link-active' : ''}`}
+                 onClick={() => {
+                     setHome(false)
+                     setCart(false)
+                     setAdd(true)
+                     setOrder(false)
+                     setProfile(false)
+                     history.push('/add')
+                 }}
+            >
+                <i className={'fa fa-plus-circle'}> </i>
+                <p>Sell</p>
+            </div>
+            <div className={`order ${order ? 'link-active' : ''}`}
+                 onClick={() => {
+                     setHome(false)
+                     setCart(false)
+                     setAdd(false)
+                     setOrder(true)
+                     setProfile(false)
+                     history.push('/messages')
+
+                 }}
+            >
+                <i className={'fab fa-facebook-messenger'}></i>
+                <p>Chats</p>
+            </div>
+            <div className={`profile ${profile ? 'link-active' : ''}`}
+                 onClick={() => {
+                     setHome(false)
+                     setCart(false)
+                     setAdd(false)
+                     setOrder(false)
+                     setProfile(true)
+                 }}
+            >
+                <img src={img} alt={''} className={''}/>
+                <p>Profile</p>
+            </div>
+        </div>
+    )
+}
+
+export default withRouter(MobileBottomNav);
